9d5eb793af37cf473ed414f2f47e11c30b339c18
rublevmaxim <rublev.maxim@mail.ru>
2020-07-31 21:45:02 +0300

    Задание 1

--- src/com/company/vtb/lesson/Main.java
+++ src/com/company/vtb/lesson/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int lenght_len=500;
+        int count_dog=0;
+        int count_cat=0;
+        int count_tiger=0;
+        int count_animal=0;
       Home_cat home_cat=new Home_cat("Барсик");
       Dog dog=new Dog("Бобик");
       Tiger tiger=new Tiger("Шархан");
@@ -20,8 +24,15 @@ public class Main {
         for(Animal zver:animal1){
             zver.run(700);
             zver.sweam(700);
-
+            count_animal++;
+            if (zver instanceof Home_cat) count_cat++;
+            if (zver instanceof Dog) count_dog++;
+            if (zver instanceof Tiger) count_tiger++;
         }
+        System.out.println("Всего животных: "+count_animal+"из них это "
+                                            +count_cat+" коты, "+
+                                            +count_dog+" собаки, "+
+                                            +count_tiger+" тигры. ");
 
     }
 }
